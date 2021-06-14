0108c118cc10f8ed02a34705b0f7615782c25644
Karthikeya Vemuri <karkeys1@uw.edu>
2021-06-13 21:58:52 -0700

    Transaction Generator created

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <map>
+#include <string>
+#include <random>
+#include <chrono>
+#include <thread>
+
+
+std::mt19937_64 rng;
+std::uniform_int_distribution<int> uni(0, 3);
+
+std::map<std::string, int> generateTransactions(int MaxVal);
 
 int main() {
+    for (int i = 2; i < 100000; i++) {
+        std::map<std::string, int> temp = generateTransactions(i);
+        std::cout << "Person1 = " << temp.find("Person1")->second << " Person2 = "
+                  << temp.find("Person2")->second << std::endl;
+
+    }
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
+
+std::map<std::string, int> generateTransactions(int MaxVal) {
+    std::uniform_int_distribution<int> distro(0, MaxVal);
+    int random_bit = uni(rng);
+    int random_integer = distro(rng);
+    int sign = (random_bit * 2) - 1;
+    int amount = random_integer * sign;
+    std::map<std::string, int> vals = {{"Person1", amount},
+                                       {"Person2", amount * -1}};
+    return vals;
+}
+
